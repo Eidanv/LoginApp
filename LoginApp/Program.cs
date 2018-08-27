0039c55c96adcc0c6c64c21225af4054b21153ea
Eidanv <dima_dmitriev@inbox.ru>
2018-08-28 00:53:03 +0600

    loginApp

--- LoginApp/Program.cs
+++ LoginApp/Program.cs
@@ -8,8 +8,47 @@ namespace LoginApp
 {
     class Program
     {
+
+        static string username;
+        static string password;
+
         static void Main(string[] args)
         {
+            registration();
+            SignIn();
+            Console.Read();
+
+        }
+
+
+        public static void registration()
+        {
+            Console.WriteLine("please create your username");
+            username = Console.ReadLine();
+            Console.WriteLine("please create your password");
+            password = Console.ReadLine();
+        }
+
+        public static void SignIn()
+        {
+            
+            Console.WriteLine("Enter your username");
+            if (username == Console.ReadLine())
+            {
+                Console.WriteLine("Enter your password");
+
+                if (password == Console.ReadLine())
+                {
+                    Console.WriteLine("Login successful");
+                }
+
+                
+            }
+            else
+            {
+                Console.WriteLine("wrong username or password, try again");
+            }
+
         }
     }
 }
